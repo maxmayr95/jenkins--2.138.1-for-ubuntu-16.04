dfff465a65a5cedb4ffe493133983ae3dff258ef
May Mayr <mmayr@fluance.ch>
2018-09-26 17:05:16 +0200

    Install java 8

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo sh -c "apt update && yes | apt install openjdk-8-jdk && java -version"
 sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
